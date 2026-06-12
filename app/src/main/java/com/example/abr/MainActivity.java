d785d713bf4d7cf5813148ed8857f5819071db14
Alsadatmission <alsadatmission82@gmail.com>
2026-06-12 21:56:52 +0600

    add new feature

--- app/src/main/java/com/example/abr/MainActivity.java
+++ app/src/main/java/com/example/abr/MainActivity.java
@@ -18,59 +18,34 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvDisplay;
-   EditText edOne;
-   Button button;
-   TextToSpeech textToSpeech;
+    Button button;
 
-    @SuppressLint("MissingSuperCall")
-    @Override
-    public void onBackPressed() {
-       // super.onBackPressed();
-        new AlertDialog.Builder(MainActivity.this)
-                .setTitle("Confirm Exit")
-                .setMessage("Do u really want to exit")
-                .setIcon(R.drawable.bird)
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                })
-                .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                        finishAndRemoveTask();
-                    }
-                })
-                .show();
-    }
+
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        edOne=findViewById(R.id.edOne);
         tvDisplay=findViewById(R.id.tvDisplay);
-
         button=findViewById(R.id.button);
-        textToSpeech= new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int i) {
 
-            }
-        });
-        
-        
-        
         button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                textToSpeech.speak("Hello How are you",TextToSpeech.QUEUE_FLUSH,null,null);
+                tvDisplay.setText("");
+                for(int x=1;x<=100;x++){
+                    tvDisplay.append("  "+  x);
+                }
             }
         });
+
+
+
+        
+        
+        
+
 
 
  
